bf0085caba4c6f64f59123bfc5c0207d4276abf7
Piyush Kukreja <piyushkukreja06@gmail.com>
2020-11-21 12:52:24 +0530

    updated HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
     public static void main(String args[]){
-        System.out.println("welcome to jenkins, job chained!");
+        System.out.println("welcome to jenkins and github");
     }
 }
